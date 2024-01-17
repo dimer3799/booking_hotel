06f442d4cb4b22ab2e89a1237dcc9a7602a600ca
dimer3799 <dimer3799@yandex.ru>
2024-01-17 21:46:54 +0500

    feat:Описание ORM таблиц в стиле SQLAlchemy 2

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,14 +1,24 @@
-from sqlalchemy import JSON, Column, Integer, String
+"""ORM модель сущности отелей"""
+
+from sqlalchemy import JSON, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
 
 class Hotels(Base):
+    """ORM модель сущности отелей"""
+
     __tablename__ = "hotels"
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    location = Column(String, nullable=False)
-    services = Column(JSON)
-    rooms_quantity = Column(Integer, nullable=False)
-    image_id = Column(Integer)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str]
+    location: Mapped[str]
+    services: Mapped[list[str]] = mapped_column(JSON)
+    rooms_quantity: Mapped[int]
+    image_id: Mapped[int]
+
+    rooms: Mapped[list["Rooms"]] = relationship(back_populates="hotel")
+
+    def __str__(self):
+        return f"Отель {self.name} {self.location[:30]}"
--- app/hotels/rooms/models.py
+++ app/hotels/rooms/models.py
@@ -1,27 +1,29 @@
+"""ORM модель сущности номера гостиниц"""
+
 from typing import Optional
 
-from sqlalchemy import JSON, Column, ForeignKey, Integer, String
+from sqlalchemy import JSON, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
 
 
 class Rooms(Base):
+    """ORM модель сущности номера гостиниц"""
+
     __tablename__ = "rooms"
 
-    id = Column(Integer, primary_key=True)
-    hotel_id = Column(ForeignKey("hotels.id"))
-    name = Column(
-        String,
-    )
-    description = Column(String, nullable=True)
-    price = Column(Integer)
-    services = Column(JSON, nullable=True)
-    quantity = Column(Integer)
-    image_id = Column(Integer)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    hotel_id: Mapped[int] = mapped_column(ForeignKey("hotels.id"))
+    name: Mapped[str]
+    description: Mapped[str]
+    price: Mapped[int]
+    services: Mapped[Optional[list[str]]] = mapped_column(JSON)
+    quantity: Mapped[int]
+    image_id: Mapped[int]
 
-    hotel = relationship("Hotels", back_populates="rooms")
-    booking = relationship("Bookings", back_populates="room")
+    hotel: Mapped["Hotels"] = relationship(back_populates="rooms")
+    booking: Mapped[list["Bookings"]] = relationship(back_populates="room")
 
     def __str__(self):
         return f"Номер {self.name}"
