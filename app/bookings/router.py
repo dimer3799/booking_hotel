71980ff838daec092be3fcebd999950187d532ae
dimer3799 <dimer3799@yandex.ru>
2024-02-07 22:58:38 +0500

    feat: Добавление методов к базовому репозиторию

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -4,19 +4,18 @@ router = APIRouter(prefix="/bookings", tags=["Bookings"])
 from sqlalchemy import select
 
 from app.bookings.dao import BookingDAO
-from app.bookings.models import Bookings
-from app.database import async_session_maker
+from app.bookings.scemas import SBooking
 
-
-@router.get("/{booking_id}")
-async def get_booking():
-    async with async_session_maker() as session:
-        query = select(Bookings)
-        result = await session.execute(query)
-        print(result)
+# @router.get("/{booking_id}")
+# async def get_booking():
+#     async with async_session_maker() as session:
+#         query = select(Bookings)
+#         result = await session.execute(query)
+#         print(result)
 
 
 @router.get("")
-async def get_bookings():
+async def get_bookings() -> list[SBooking]:
     result = await BookingDAO.find_all()
+
     return result
---
+++ app/bookings/scemas.py
@@ -0,0 +1,18 @@
+"""Описание схемы бронирования"""
+
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class SBooking(BaseModel):
+    """Схема бронирования номеров"""
+
+    id: int
+    room_id: int
+    user_id: int
+    date_from: date
+    date_to: date
+    price: int
+    total_cost: int
+    total_days: int
